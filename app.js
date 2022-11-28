23f8a8314a3ac1fe395914d7d59955fa3aac9c02
Kyle Henderson <81394947+tester242@users.noreply.github.com>
2022-11-27 22:43:35 -0500

    setup continued

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const configRoutes = require('./routes');
+const exphbs = require('express-handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 configRoutes(app);
 app.listen(3000, async () => {
